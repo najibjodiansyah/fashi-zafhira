ea7e3db7a56ad5872ba74d878aea3ca877bcaae1
najibjodiansyah <najibjodiansyah11@gmail.com>
2024-07-21 11:21:19 +0700

    init project learn laravue 8 - 2.6

---
+++ be/readme.md
@@ -0,0 +1,1 @@
+backend repository for fashi-zafhira
---
+++ fe/src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <HeaderSection />
+    <HeroSection />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderSection from '@/components/HeaderSection.vue'
+import HeroSection from '@/components/HeroSection.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    // HelloWorld
+    HeaderSection,
+    HeroSection
+  },
+}
+</script>
